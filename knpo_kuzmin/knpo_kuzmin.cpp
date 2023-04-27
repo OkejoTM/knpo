844548c0805c1f6799b24403499fab821bb02832
OkejoTM <denastytiko03@mail.ru>
2023-04-27 18:11:40 +0300

    add read from input file

--- knpo_kuzmin/knpo_kuzmin.cpp
+++ knpo_kuzmin/knpo_kuzmin.cpp
@@ -1,13 +1,91 @@
 ﻿#include <iostream>
-
-
-
-
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <queue>
 
 using namespace std;
 
+
+vector<string> readDataFromFile(vector<vector<int>>& roads, vector<int>& costs)
+{
+	//открытие файла
+	ifstream file;
+	file.open("input.csv");
+
+	//считывание первой строки файла
+	string line;
+	getline(file, line);
+	stringstream ss(line);
+
+	string label;
+	vector<string> citiesLabes;
+	
+
+	//заполнить вектор названиями городов
+	while (getline(ss, label, ';')) {
+		if (label != "") {
+			citiesLabes.push_back(label);
+		}
+	}
+	roads.resize(citiesLabes.size(), vector<int>(citiesLabes.size(), 0));
+	costs.resize(citiesLabes.size(), 0);
+
+	getline(file, line);
+	stringstream ssCost(line);		
+	int costValueCol = 0;
+	string costVal;
+
+	// Считывание стоимости дорог
+	while (getline(ssCost, costVal, ';')) {
+		if (costVal != "") {
+			costs[costValueCol] = stoi(costVal);
+		}
+		costValueCol++;
+	}
+
+	getline(file, line); // Считать пустую строку
+	getline(file, line); // Считать названия город
+
+	//заполнение матрицы смежности из файла
+	int row = 0;
+	while (getline(file, line)) {
+		stringstream ss(line);
+		string value;
+		int col = 0;
+		while (getline(ss, value, ';')) {
+			if (value != "" && col > 0) {
+				roads[row][col - 1] = stoi(value);
+			}
+			col++;
+		}
+		row++;
+	}
+
+	file.close();
+
+	return citiesLabes;
+}
+
 int main() {
-	const int N = 5;
+	
+	vector<vector<int>> matrixRoads;
+	vector<int> costs;
+	auto labels = readDataFromFile(matrixRoads, costs);
+	
+	for (int i = 0; i < costs.size(); i++) {
+		cout << costs[i] << " ";
+	}
+	cout << endl;
+
+	/*for (int i = 0; i < matrixRoads.size(); i++) {
+		for (int j = 0; j < matrixRoads.size(); j++) {
+
+		}
+	}*/
+
+	/*const int N = 5;
 
 	int costs[2][N];
 
@@ -45,6 +123,8 @@ int main() {
 
 
 
+
+
 	cout << endl;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
@@ -59,7 +139,7 @@ int main() {
 			cout << costs[i][j] << " ";
 		}
 		cout << endl;
-	}
+	}*/
 
 
 	return 0;
