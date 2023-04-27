a533f7d5efcae5c85bace5f4e1babc1d573fa220
OkejoTM <denastytiko03@mail.ru>
2023-04-27 14:49:38 +0300

    add first read/change/write matrix of roads

--- knpo_kuzmin/knpo_kuzmin.cpp
+++ knpo_kuzmin/knpo_kuzmin.cpp
@@ -2,23 +2,65 @@
 
 
 
-/*
-
-	1 2 3 4 5 6
-  1 0 1 9 0 2 4
-  2 0 0 3 4 2 1
-  3 
-  4 
-  5 
-  6 
-
-
-*/
 
 
 using namespace std;
 
 int main() {
+	const int N = 5;
+
+	int costs[2][N];
+
+	cout << "Enter costs of each city liters of fuel:\n";
+	int i = 0;
+
+	for (int j = 0; j < N; j++) 
+	{
+		costs[i][j] = j + 1;
+		cout << j+1 << "city:";
+		cin >> costs[i+1][j];
+	}
+
+	int roads[N][N];
+
+	cout << "Enter roads:\n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> roads[i][j];
+		}
+	}
+
+	int roadsWithCosts[N][N];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			roadsWithCosts[i][j] = roads[i][j] * costs[1][i];
+			roadsWithCosts[j][i] = roads[j][i] * costs[1][j];
+		}
+	}
+
+
+
+
+
+
+
+	cout << endl;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << roadsWithCosts[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "costs: \n";
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << costs[i][j] << " ";
+		}
+		cout << endl;
+	}
+
 
 	return 0;
 }
